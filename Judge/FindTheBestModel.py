7a9bde96249ada1671708bbbb916d602df6a1b26
Lewis-panda <116704255+Lewis-panda@users.noreply.github.com>
2024-07-25 02:40:34 +0800

    Update FindTheBestModel.py

--- Judge/FindTheBestModel.py
+++ Judge/FindTheBestModel.py
@@ -6,11 +6,9 @@ def calculate_total_and_average_scores(df):
 
     for model in models:
         model_df = df[df['Model'] == model]
-        # 计算各项评分的总和作为总分
         total_score = model_df[['清晰度_score', '保留原意_score', '描述性_score', '深度_score',
                                 '觀點多樣性_score', '平衡性_score', '詞彙多樣性_score', '意思保留_score',
                                 '聚焦性_score', '強調點_score']].sum(axis=1).sum()
-        # 计算各项评分的平均分
         average_score = model_df[['清晰度_score', '保留原意_score', '描述性_score', '深度_score',
                                   '觀點多樣性_score', '平衡性_score', '詞彙多樣性_score', '意思保留_score',
                                   '聚焦性_score', '強調點_score']].sum(axis=1).mean()
@@ -23,16 +21,12 @@ def calculate_total_and_average_scores(df):
     return pd.DataFrame(results)
 
 def main():
-    # 读取 evaluation_result.csv 文件
     df = pd.read_csv('evaluation_result.csv', encoding='utf-8-sig')
 
-    # 计算每个模型的总分和平均分
     results_df = calculate_total_and_average_scores(df)
     
-    # 保存结果到 CSV 文件
     results_df.to_csv('model_performance.csv', index=False, encoding='utf-8-sig')
     
-    # 输出结果
     print(results_df)
 
 if __name__ == "__main__":
