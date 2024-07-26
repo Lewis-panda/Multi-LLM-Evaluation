8ad843430d8292a2e5ba54b7c6d302d29101fea6
Lewis-panda <116704255+Lewis-panda@users.noreply.github.com>
2024-07-26 17:22:26 +0800

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# 定义虚拟环境路径
+# 定義虛擬環境路徑
 ENV_PATH="/Users/lewis611036/DataAugmentation/DAenv/bin/activate"
 WORK_DIR="/Users/lewis611036/DataAugmentation/Run_Models"
 
-# 移除现有的 mysession tmux 会话
+# 移除現有的 mysession tmux 會話
 tmux kill-session -t mysession 2>/dev/null
 
-# 创建一个 AppleScript 来打开新的终端窗口并运行 tmux 命令
+# 創建一個 AppleScript 來打開新的終端窗口並運行 tmux 命令
 osascript <<EOF
 tell application "Terminal"
     do script "
@@ -23,7 +23,7 @@ tell application "Terminal"
     tmux split-window -v -t mysession:0.0
     tmux send-keys -t mysession 'source $ENV_PATH; python3 run_llama3.py' C-m
 
-    # 分割畫面並運行 run_qwen.py
+    # 分割畫面並運行 run_qwen2.py
     tmux split-window -v -t mysession:0.1
     tmux send-keys -t mysession 'source $ENV_PATH; python3 run_qwen2.py' C-m
 
@@ -35,13 +35,21 @@ tell application "Terminal"
     tmux split-window -h -t mysession:0.3
     tmux send-keys -t mysession 'source $ENV_PATH; python3 run_deepseek.py' C-m
 
+    # 分割畫面並運行 run_mistral-Large.py
+    tmux split-window -v -t mysession:0.0
+    tmux send-keys -t mysession 'source $ENV_PATH; python3 run_mistral-Large.py' C-m
+
+    # 分割畫面並運行 run_llama3.1_70B.py
+    tmux split-window -h -t mysession:0.4
+    tmux send-keys -t mysession 'source $ENV_PATH; python3 run_llama3.1_70B.py' C-m
+
     # 確保所有窗格均勻排列
     tmux select-layout -t mysession tiled
     tmux -2 attach-session -t mysession
     "
-    # 最大化终端窗口
+    # 最大化終端窗口
 #    set bounds of front window to {0, 0, 1440, 900}
-    # 将终端窗口设为全屏
+    # 將終端窗口設為全屏
     tell application "System Events"
         keystroke "f" using {control down, command down}
     end tell
